ccfc79591b6a7473ff41a4ec949b0c6af310b5d5
willpeely <william.e.peel@icloud.com>
2025-05-17 19:25:42 +0000

    Setting convolutional variables

--- CNN.py
+++ CNN.py
@@ -4,3 +4,10 @@ import torch.optim as optim
 import torchvision.transforms as tranforms
 from torch.utils.data import DataLoader
 import time
+
+# Setting the variables for convolutional layers
+convolutional_kernel_size = 3    
+convolutional_stride = 1
+convolutional_padding = 1
+convolutional_activation = nn.ReLU()
+convolutional_pool = nn.MaxPool2d(kernel_size=2, stride=2)
