003d47fb6f9883247760a73a71a1d09e1dfe0efc
Will Peel <william.e.peel@icloud.com>
2025-11-09 17:25:21 +0000

    Updated website

---
+++ script.js
@@ -0,0 +1,14 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((button) => {
+    button.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+        content.style.maxHeight = null; 
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px"; 
+        }
+    });
+});
